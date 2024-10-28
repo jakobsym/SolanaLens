f4b7639fc722777a9460cbf9905dc12556628646
jakobsym <jakob.symchych@my.utsa.edu>
2024-10-28 10:12:12 -0500

    setup fetchWalletContent() to use helius SDK methods. Correctly iterates over paginated API returning all wallets with respective amounts. Still need to format amount response and make it more readable.

--- api/src/app.js
+++ api/src/app.js
@@ -3,13 +3,83 @@ import 'dotenv/config'
 
 const helius = new Helius(process.env.HELIUS_API_KEY)
 
-const response = await helius.rpc.getTokenAccounts({
-  page: 1,
-  limit: 100,
-  options: {
-    showZeroBalance: false,
-  },
-  owner: "3kYg4HFn9PFG3PBUjSsuCEiZb3zDxnrbuQQnY1iUjy5f"
-});
+// walletAddress: String
+// return: Object 
+/* 
+- amount will be formatted to be readable
+  "walletAddress": "0x123",
+  "tokens": [
+    {
+      "tokenAddress": "0x123",
+      "amount": xyz
+    }
+  ]
+}
+*/
+// TODO: Pass in a wallet address as parameter
+const fetchWalletContent = async() => {
+  var totalPages = 0
 
-console.log(response)
+  var walletContent = {
+    walletAddress: "",
+    tokens: [],
+  }
+
+  // TODO: Maybe split these into seperate functions
+  try {
+    let initRes = await helius.rpc.getTokenAccounts({
+      page: 1,
+      limit: 100,
+      options: {
+        showZeroBalance: false
+      },
+      owner: process.env.TEST_ADDRESS   //walletAddress
+    });
+
+    // get total pages for given addresss
+    totalPages = initRes.total
+    
+    for (const token of initRes.token_accounts) {
+      walletContent.tokens.push({
+        "token_address": token.mint,
+        "amount": token.amount
+      })
+      // initRes[tpData] gives :value
+    }
+    // capture rest of data
+  } catch (error) {
+    console.error(error)
+  }
+
+  
+  // iterate over all remaining pages capturing data
+  for (let page = 2; page <= totalPages; totalPages+=1) {
+    try {
+      let res = await helius.rpc.getTokenAccounts({
+        page: page,
+        limit: 100,
+        options: {
+          showZeroBalance: false
+        },
+        owner: process.env.TEST_ADDRESS
+      })
+
+      for (const token of res.token_accounts) {
+        walletContent.tokens.push({
+          "token_address": token.mint,
+          "amount": token.amount,
+        })
+      }
+
+    } catch (error) {
+      console.error(error)
+      break
+    }
+  }
+
+  return walletContent
+}
+
+
+const wallet = await fetchWalletContent()
+console.log(wallet)
