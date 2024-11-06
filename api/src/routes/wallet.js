f8170818a865052986b88f2232e71904f0e38a3f
Jakob <jakob.symchych@my.utsa.edu>
2024-11-06 14:58:37 -0700

    pushing so I can work on another machine

--- api/src/routes/wallet.js
+++ api/src/routes/wallet.js
@@ -2,29 +2,20 @@ import Wallet from '../models/Wallet.js'
 import {fetchWalletContent} from '../utils/helius.js'
 const walletRoutes = (fastify, options) => {
     
-    // call fetchWalletContent from helius.js
-    // middleware will clean the amount 
-    // before it gets sent to client as JSON
     //TODO: Create a validation function on param
     fastify.get('/:walletAddress', async(req, res) => {
         try {
             const walletAddress = req.params
             const tokens = fetchWalletContent(walletAddress)
-            var wallet = new Wallet(walletAddress, tokens)            
-            res
-                .code(200)
-                .header()
-                .send(wallet)
+            var wallet = new Wallet(walletAddress, tokens)         
+            // TODO: before wallet of tokens gets sent back to user
+            //      the amounts of each token need to be cleaned   
+            res.code(200).header('Content-Type', 'application/json').send(wallet)
         } catch (error) {
-            
+            fastify.log.error({error, params: req.params}, 'Error in wallet route')
+            res.code(500).send({error: 'Unexpected error occurred'})
         }
-
-
-        return wallet
     })
-
-
-
 }
 
 export default walletRoutes;
