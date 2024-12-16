c5fb519d1ed7e8ea0d751dcc650c971df82f7046
jakobsym <jakob.symchych@my.utsa.edu>
2024-12-16 08:03:28 -0600

    init file

---
+++ api/src/utils/solscan.js
@@ -0,0 +1,9 @@
+
+/* 
+- Returns an object {token_name: "XYZ", token_symbol: "ABC", token_creator: "XYZ"}
+*/
+export const fetchTokenMetadata = async(tokenAddressObj) => {
+    const tokenAddress = tokenAddressObj.tokenAddress;
+
+}
+
